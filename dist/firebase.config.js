a99587bdfd6943dc5ffa41777d3f1f6e91c0d5ab
adris-h <adrianahanouskova2007@gmail.com>
2025-12-29 16:39:36 +0100

    update 16/12/2025 - form

---
+++ dist/firebase.config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDx7v_ex7fJzQLhJ_NF0Vp6Qe4lei-BZ80",
+    authDomain: "svatba-mejstky.firebaseapp.com",
+    projectId: "svatba-mejstky",
+    storageBucket: "svatba-mejstky.firebasestorage.app",
+    messagingSenderId: "2117358990",
+    appId: "1:2117358990:web:23ea6a243f031db00000df",
+    measurementId: "G-3DDT16GHN4"
+};
